b668da805b86417ee3c2623362426c770a11941d
Hasunie Adikari <hasunie@wso2.com>
2023-08-22 00:01:26 +0530

    formatted code

--- portals/admin/src/main/webapp/source/src/app/components/Base/Navigator.jsx
+++ portals/admin/src/main/webapp/source/src/app/components/Base/Navigator.jsx
@@ -98,7 +98,6 @@ function Navigator(props) {
 
     const entireArray = [];
     const checkRouteMenuMapping = routeMenuMapping;
-    console.log('-----mapping-----------', JSON.stringify(routeMenuMapping));
     for (let i = 0; i < checkRouteMenuMapping.length; i++) {
         const adminRoute = routeMenuMapping;
         if (checkRouteMenuMapping[i].id === 'Dashboard') {
@@ -152,7 +151,6 @@ function Navigator(props) {
         }
         routeMenuMapping = entireArray;
         if (_scopes.includes('apim:admin')) {
-            console.log('-----mapping-----------', JSON.stringify(routeMenuMapping));
             routeMenuMapping = adminRoute;
         }
     }
